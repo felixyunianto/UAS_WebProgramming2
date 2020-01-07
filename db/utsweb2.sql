59ac80aaf181e9cd01545b7c346d8154d62f312f
felixyunianto <felixyunianto@gmail.com>
2020-01-07 21:43:57 +0700

    first push

--- db/utsweb2.sql
+++ db/utsweb2.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Jan 03, 2020 at 09:33 PM
+-- Generation Time: Dec 12, 2019 at 08:12 AM
 -- Server version: 5.7.28-0ubuntu0.18.04.4
--- PHP Version: 7.2.26-1+ubuntu18.04.1+deb.sury.org+1
+-- PHP Version: 7.2.25-1+ubuntu18.04.1+deb.sury.org+1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -28,22 +28,21 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `absen` (
   `id_absen` int(10) NOT NULL,
+  `nim` char(8) NOT NULL,
+  `nama_mahasiswa` varchar(50) NOT NULL,
+  `nidn` int(11) NOT NULL,
+  `nama_dosen` varchar(255) NOT NULL,
   `id_matkul` int(10) NOT NULL,
-  `nim` int(10) NOT NULL,
-  `status` tinyint(1) NOT NULL DEFAULT '0',
-  `tanggal` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+  `nama_matkul` varchar(50) NOT NULL,
+  `tanggal_masuk` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `absen`
 --
 
-INSERT INTO `absen` (`id_absen`, `id_matkul`, `nim`, `status`, `tanggal`) VALUES
-(1, 1, 17090121, 1, '2019-12-26 15:17:42'),
-(2, 1, 1, 1, '2019-12-26 15:23:41'),
-(3, 1, 1, 1, '2019-12-26 15:25:17'),
-(4, 3, 17090121, 1, '2020-01-01 02:33:49'),
-(5, 1, 17090121, 1, '2020-01-01 02:42:29');
+INSERT INTO `absen` (`id_absen`, `nim`, `nama_mahasiswa`, `nidn`, `nama_dosen`, `id_matkul`, `nama_matkul`, `tanggal_masuk`) VALUES
+(2, '17090121', 'Felix Yunianto Gunawan', 40121, 'M. Muzaqi, S.Kom', 1, 'Web Programming 2', '2019-11-06 12:08:51');
 
 -- --------------------------------------------------------
 
@@ -54,7 +53,6 @@ INSERT INTO `absen` (`id_absen`, `id_matkul`, `nim`, `status`, `tanggal`) VALUES
 CREATE TABLE `admin` (
   `id` int(11) NOT NULL,
   `username` varchar(50) NOT NULL,
-  `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `nama` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -63,8 +61,8 @@ CREATE TABLE `admin` (
 -- Dumping data for table `admin`
 --
 
-INSERT INTO `admin` (`id`, `username`, `email`, `password`, `nama`) VALUES
-(1, 'admin', 'admin@gmail.com', 'admin', 'Administrator');
+INSERT INTO `admin` (`id`, `username`, `password`, `nama`) VALUES
+(1, 'admin', 'admin', 'Administrator');
 
 -- --------------------------------------------------------
 
@@ -75,23 +73,16 @@ INSERT INTO `admin` (`id`, `username`, `email`, `password`, `nama`) VALUES
 CREATE TABLE `dosen` (
   `nidn` int(11) NOT NULL,
   `nama` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
   `mata_kuliah` varchar(255) NOT NULL,
-  `alamat` text NOT NULL,
-  `deleted` enum('undeleted','deleted') NOT NULL
+  `alamat` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `dosen`
 --
 
-INSERT INTO `dosen` (`nidn`, `nama`, `email`, `password`, `mata_kuliah`, `alamat`, `deleted`) VALUES
-(40121, 'M. Muzaqi, S.Kom', 'muzaqi@gmail.com', '123456', 'Web Programming 2', 'Gatau', 'undeleted'),
-(40122, 'Nurul Renaningtiyas, M.Kom', 'nurulrenaningtiyas@gmail.com', '123456', 'Enterprise Resource Planning', 'Gatau', 'undeleted'),
-(40124, 'Slamet Wiyono, S.Pd, M.Eng', 'slamet@gmail.com', '123456', 'E-Business and E-Commerce', 'Gatau', 'undeleted'),
-(40125, 'Sena Wijayanto, S.Pd, M.T', 'sena@gmail.com', '123456', 'Object Oriented Programming 2', 'Gatau', 'undeleted'),
-(40126, 'Priyanto Tamami, S.Kom', 'tamami@gmail.com', '123456', 'Data Warehouse', 'Gatau', 'deleted');
+INSERT INTO `dosen` (`nidn`, `nama`, `mata_kuliah`, `alamat`) VALUES
+(40121, 'M. Muzaqi, S.Kom', '', 'Gatau');
 
 -- --------------------------------------------------------
 
@@ -100,23 +91,25 @@ INSERT INTO `dosen` (`nidn`, `nama`, `email`, `password`, `mata_kuliah`, `alamat
 --
 
 CREATE TABLE `mahasiswa` (
-  `nim` int(8) NOT NULL,
+  `nim` char(8) NOT NULL,
   `nama` varchar(50) NOT NULL,
   `kelas` varchar(2) NOT NULL,
   `email` varchar(50) NOT NULL,
-  `password` varchar(255) NOT NULL,
   `no_hp` int(14) NOT NULL,
   `alamat` varchar(255) NOT NULL,
-  `semester` int(1) NOT NULL,
-  `deleted` enum('undeleted','deleted') NOT NULL
+  `semester` int(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `mahasiswa`
 --
 
-INSERT INTO `mahasiswa` (`nim`, `nama`, `kelas`, `email`, `password`, `no_hp`, `alamat`, `semester`, `deleted`) VALUES
-(17090001, 'Felix Yunianto Gunawan', 'B', 'felixyunianto@gmail.com', 'felix12345', 87473657, 'PAI', 8, 'undeleted');
+INSERT INTO `mahasiswa` (`nim`, `nama`, `kelas`, `email`, `no_hp`, `alamat`, `semester`) VALUES
+('17090017', 'Fitria Nurizqi Handayani', 'D', 'fitriarizqi07@gmail.com', 875614658, 'Wangandawa', 5),
+('17090051', 'Desi Ayu Purwono', 'B', 'desipurwono@gmail.com', 12345678, 'Mbuh Kue', 5),
+('17090110', 'Diki Priyanto', 'B', 'dhikipriyanto@gmail.com', 12345678, 'Bongkok', 5),
+('17090121', 'Felix Yunianto Gunawan', 'B', 'felixyunianto@gmail.com', 1234567890, 'PAI', 5),
+('6', 'asdhjkh', 'A', 'a@gmail.com', 81273871, 'lsfh', 8);
 
 -- --------------------------------------------------------
 
@@ -136,21 +129,7 @@ CREATE TABLE `matkul` (
 --
 
 INSERT INTO `matkul` (`id_matkul`, `nama_matkul`, `nidn`, `semester`) VALUES
-(1, 'Web Programming 2', 40121, 5),
-(2, 'Framework PHP', 40121, 6),
-(3, 'Database', 40122, 5);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `reset_password`
---
-
-CREATE TABLE `reset_password` (
-  `id` int(10) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `code` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+(1, 'Web Programming 2', 40121, 5);
 
 --
 -- Indexes for dumped tables
@@ -160,7 +139,13 @@ CREATE TABLE `reset_password` (
 -- Indexes for table `absen`
 --
 ALTER TABLE `absen`
-  ADD PRIMARY KEY (`id_absen`);
+  ADD PRIMARY KEY (`id_absen`),
+  ADD KEY `nim` (`nim`),
+  ADD KEY `nama_mahasiswa` (`nama_mahasiswa`),
+  ADD KEY `nidn` (`nidn`),
+  ADD KEY `nama_dosen` (`nama_dosen`),
+  ADD KEY `id_matkul` (`id_matkul`),
+  ADD KEY `nama_matkul` (`nama_matkul`);
 
 --
 -- Indexes for table `admin`
@@ -191,12 +176,6 @@ ALTER TABLE `matkul`
   ADD KEY `nidn` (`nidn`);
 
 --
--- Indexes for table `reset_password`
---
-ALTER TABLE `reset_password`
-  ADD PRIMARY KEY (`id`);
-
---
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -204,27 +183,27 @@ ALTER TABLE `reset_password`
 -- AUTO_INCREMENT for table `absen`
 --
 ALTER TABLE `absen`
-  MODIFY `id_absen` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id_absen` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `admin`
 --
 ALTER TABLE `admin`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
--- AUTO_INCREMENT for table `dosen`
+-- Constraints for dumped tables
 --
-ALTER TABLE `dosen`
-  MODIFY `nidn` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40127;
+
 --
--- AUTO_INCREMENT for table `mahasiswa`
+-- Constraints for table `absen`
 --
-ALTER TABLE `mahasiswa`
-  MODIFY `nim` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17090002;
---
--- AUTO_INCREMENT for table `reset_password`
---
-ALTER TABLE `reset_password`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `absen`
+  ADD CONSTRAINT `absen_ibfk_1` FOREIGN KEY (`nim`) REFERENCES `mahasiswa` (`nim`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `absen_ibfk_2` FOREIGN KEY (`nidn`) REFERENCES `dosen` (`nidn`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `absen_ibfk_3` FOREIGN KEY (`nama_mahasiswa`) REFERENCES `mahasiswa` (`nama`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `absen_ibfk_5` FOREIGN KEY (`id_matkul`) REFERENCES `matkul` (`id_matkul`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `absen_ibfk_6` FOREIGN KEY (`nama_matkul`) REFERENCES `matkul` (`nama_matkul`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `absen_ibfk_7` FOREIGN KEY (`nama_dosen`) REFERENCES `dosen` (`nama`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
